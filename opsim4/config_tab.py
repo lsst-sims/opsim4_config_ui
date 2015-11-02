4a0853200dda51d38567d16bd86559cfad2f3c31
Michael Reuter <mareuternh@gmail.com>
2015-11-02 12:39:13 -0700

    Making UI show modifications and only saving those to file.

--- opsim4/config_tab.py
+++ opsim4/config_tab.py
@@ -14,6 +14,9 @@ class ConfigurationTab(QtGui.QWidget):
         self.config_cls = load_class(self.config_obj)
 
         self.layout = QtGui.QGridLayout()
+        self.signal_mapper = QtCore.QSignalMapper(self)
+        self.signal_mapper.mapped[QtGui.QWidget].connect(self.check_property)
+
         self.create_form()
         self.setLayout(self.layout)
 
@@ -47,6 +50,8 @@ class ConfigurationTab(QtGui.QWidget):
                 self.layout.addWidget(name_label, i, 0)
                 pwidget = self.make_property_widget(tcls, v)
                 name_label.setBuddy(pwidget)
+                pwidget.editingFinished.connect(self.signal_mapper.map)
+                self.signal_mapper.setMapping(pwidget, pwidget)
                 self.layout.addWidget(pwidget, i, 1)
                 unit_label = QtGui.QLabel("")
                 self.layout.addWidget(unit_label, i, 2)
@@ -54,31 +59,66 @@ class ConfigurationTab(QtGui.QWidget):
     def make_property_widget(self, tcls, v):
         widget = QtGui.QWidget()
         if tcls == "Str":
-            widget = QtGui.QLineEdit(self.config_obj.toDict()[v.name])
+            widget = QtGui.QLineEdit(self.get_dict_value(v.name))
         if tcls == "Float":
-            widget = QtGui.QLineEdit(str(self.config_obj.toDict()[v.name]))
+            widget = QtGui.QLineEdit(str(self.get_dict_value(v.name)))
             widget.setValidator(QtGui.QDoubleValidator())
 
         widget.setToolTip(v.doc)
         return widget
 
-    def save(self, save_dir):
-        filename = self.tab_name + ".py"
-        with open(os.path.join(save_dir, filename), 'w') as ofile:
-            ofile.write("import {}".format(self.config_cls.__module__))
-            ofile.write(os.linesep)
-            ofile.write("assert type(config)=={0}.{1}, \'config is of type %s.%s instead of {0}.{1}\' % "
-                        "(type(config).__module__, type(config).__name__)".format(self.config_cls.__module__,
-                                                                                  self.config_cls.__name__))
-            ofile.write(os.linesep)
+    def get_dict_value(self, name):
+        return self.config_obj.toDict()[name]
 
-            for i in range(self.layout.rowCount()):
-                property_label = self.layout.itemAtPosition(i, 0).widget()
-                property_name = str(property_label.text())
-                property_value = self.config_obj.toDict()[property_name]
-                if isinstance(property_value, str):
-                    property_format = "config.{}=\'{}\'"
-                else:
+    def check_property(self, pwidget):
+        pos = self.layout.indexOf(pwidget)
+        plabel = self.layout.itemAt(pos - 1).widget()
+        pname = str(plabel.text())
+
+        try:
+            v1 = float(pwidget.text())
+        except ValueError:
+            v1 = pwidget.text()
+        v2 = self.get_dict_value(pname)
+        #print("{}, {}".format(v1, type(v1)))
+        #print("{}, {}".format(v2, type(v2)))
+        if v1 != v2:
+            #print("{} finished editing.".format(pname))
+            changed_label = "{}*".format(pname)
+            plabel.setText(changed_label)
+            self.change_label_color(plabel, QtCore.Qt.red)
+
+    def change_label_color(self, label, color):
+        palette = label.palette()
+        palette.setColor(label.foregroundRole(), color)
+        label.setPalette(palette)
+
+    def save(self, save_dir):
+        changed_values = []
+        for i in range(self.layout.rowCount()):
+            property_label = self.layout.itemAtPosition(i, 0).widget()
+            property_name_mod = str(property_label.text())
+            if property_name_mod.endswith('*'):
+                property_name = property_name_mod.strip('*')
+                property_widget = self.layout.itemAtPosition(i, 1).widget()
+                try:
+                    property_value = float(property_widget.text())
                     property_format = "config.{}={}"
-                ofile.write(property_format.format(property_name, property_value))
+                except ValueError:
+                    property_value = str(property_widget.text())
+                    property_format = "config.{}=\'{}\'"
+
+                changed_values.append(property_format.format(property_name, property_value))
+                changed_values.append(os.linesep)
+
+        if len(changed_values) != 0:
+            filename = self.tab_name + ".py"
+            with open(os.path.join(save_dir, filename), 'w') as ofile:
+                ofile.write("import {}".format(self.config_cls.__module__))
                 ofile.write(os.linesep)
+                ofile.write("assert type(config)=={0}.{1}, \'config is of type %s.%s instead of {0}.{1}\' % "
+                            "(type(config).__module__, type(config).__name__)"
+                            "".format(self.config_cls.__module__, self.config_cls.__name__))
+                ofile.write(os.linesep)
+                for line in changed_values:
+                    ofile.write(line)
