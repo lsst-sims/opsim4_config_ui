7766f57908551cc3deb9c47f4be017216c79cc88
Michael Reuter <mareuter@lsst.org>
2016-08-01 21:09:24 -0700

    Fixing order of ObservatoryModel components.

--- opsim4/model/observatory_model.py
+++ opsim4/model/observatory_model.py
@@ -1,3 +1,5 @@
+import collections
+
 from opsim4.model import CameraModel, DomeModel, ObservatoryVariationModel
 from opsim4.model import OpticsLoopCorrModel, ParkModel, RotatorModel
 from opsim4.model import SlewModel, TelescopeModel
@@ -11,12 +13,12 @@ class ObservatoryModel(object):
     def __init__(self):
         """Initialize the class.
         """
-        self.params = {}
+        self.params = collections.OrderedDict()
         self.params["telescope"] = TelescopeModel()
         self.params["dome"] = DomeModel()
-        self.params["camera"] = CameraModel()
         self.params["rotator"] = RotatorModel()
-        self.params["slew"] = SlewModel()
+        self.params["camera"] = CameraModel()
         self.params["optics_loop_corr"] = OpticsLoopCorrModel()
+        self.params["slew"] = SlewModel()
         self.params["park"] = ParkModel()
-        self.params["obs_variation"] = ObservatoryVariationModel()
+        self.params["obs_var"] = ObservatoryVariationModel()
