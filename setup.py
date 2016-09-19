d06234a12421e2fc7a6f3533a25a5243a081fa2d
Michael Reuter <mareuter@lsst.org>
2016-09-19 10:58:09 -0700

    Setting up resource building.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
-from setuptools import setup
+from setuptools import Command, setup
+import stat
+import subprocess
 
 PACKAGE = 'opsim4'
 
@@ -10,6 +12,46 @@ PATCH = 0
 MODULE = "lsst.sims.{}".format(PACKAGE)
 
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, PATCH)
+
+def is_newer(src, target):
+    """Function to check timestamps for file creation
+    """
+    if not os.path.exists(target):
+        return True
+    src_mtime = os.stat(src)[stat.ST_MTIME]
+    target_mtime = os.stat(target)[stat.ST_MTIME]
+    return src_mtime > target_mtime
+
+def exec_cmd(cmd):
+    """Function to run command calls
+    """
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, _) = proc.communicate()
+    proc.wait()
+    if proc.returncode:
+        print(stdout)
+
+class BuildRccCommand(Command):
+    description = "Build PyQt resources"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        qtr = "image_resources.qrc"
+        parts = MODULE.split('.')
+        parts.append("image_resources.py")
+        pyqtr = os.path.join(*parts)
+
+        if is_newer(qtr, pyqtr):
+            # print(qtr, pyqtr)
+            pyrcc_cmd = ["pyrcc4", "-o", "{}".format(pyqtr), "{}".format(qtr)]
+            print(" ".join(pyrcc_cmd))
+            exec_cmd(pyrcc_cmd)
 
 def write_version(filename="version.py"):
     parts = MODULE.split('.')
@@ -30,6 +72,7 @@ if __name__ == '__main__':
           url='https://github.com/lsst-sims/opsim4_config_ui',
           license='MIT',
           long_description=os.linesep + open("README.rst").read(),
+          cmdclass={'build_rcc': BuildRccCommand},
           packages=[
               'lsst',
           ],
