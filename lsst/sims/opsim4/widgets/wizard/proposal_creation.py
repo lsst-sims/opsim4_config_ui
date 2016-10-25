f215b159451b15e738dcee743b23177493907039
Michael Reuter <mareuter@lsst.org>
2016-10-25 14:28:56 -0700

    Fixing indentation issues.

--- lsst/sims/opsim4/widgets/wizard/proposal_creation.py
+++ lsst/sims/opsim4/widgets/wizard/proposal_creation.py
@@ -9,6 +9,8 @@ from lsst.sims.opsim4.widgets.wizard import SkyExclusionPage, SkyNightlyBoundsPa
 
 __all__ = ["ProposalCreationWizard"]
 
+PADDING = "    "
+
 class ProposalCreationWizard(QtGui.QWizard):
     """Main class for proposal creation wizard.
     """
@@ -50,7 +52,7 @@ class ProposalCreationWizard(QtGui.QWizard):
     def accept(self):
         """Process the given information.
         """
-        prop_save_dir = os.path.join(self.save_directory, "new_props")
+        prop_save_dir = os.path.join(str(self.save_directory), "new_props")
         if not os.path.exists(prop_save_dir):
             os.mkdir(prop_save_dir)
 
@@ -86,15 +88,15 @@ class ProposalCreationWizard(QtGui.QWizard):
         prop_file_lines.append(os.linesep)
         prop_file_lines.append("class {}({}):".format(full_prop_name, prop_type))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\tdef setDefaults(self):")
+        prop_file_lines.append("{}def setDefaults(self):".format(PADDING))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\tself.name = \"{}\"".format(full_prop_name))
+        prop_file_lines.append("{}self.name = \"{}\"".format(PADDING * 2, full_prop_name))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# -------------------------")
+        prop_file_lines.append("{}# -------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# Sky Region specifications")
+        prop_file_lines.append("{}# Sky Region specifications".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# -------------------------")
+        prop_file_lines.append("{}# -------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
 
         sky_regions_selections = str(self.field("sky_region_selections").toString()).strip()
@@ -103,117 +105,130 @@ class ProposalCreationWizard(QtGui.QWizard):
             selection_obj = "sel{}".format(i)
             selection_list.append("{}: {}".format(i, selection_obj))
             parts = sky_region_selection.split(',')
-            prop_file_lines.append("\t{} = Selection()".format(selection_obj))
+            prop_file_lines.append("{}{} = Selection()".format(PADDING * 2, selection_obj))
             prop_file_lines.append(os.linesep)
-            prop_file_lines.append("\t{}.limit_type = \"{}\"".format(selection_obj, parts[0]))
+            prop_file_lines.append("{}{}.limit_type = \"{}\"".format(PADDING * 2, selection_obj, parts[0]))
             prop_file_lines.append(os.linesep)
-            prop_file_lines.append("\t{}.minimum_limit = {}".format(selection_obj, float(parts[1])))
+            prop_file_lines.append("{}{}.minimum_limit = {}".format(PADDING * 2, selection_obj,
+                                                                    float(parts[1])))
             prop_file_lines.append(os.linesep)
-            prop_file_lines.append("\t{}.maximum_limit = {}".format(selection_obj, float(parts[2])))
+            prop_file_lines.append("{}{}.maximum_limit = {}".format(PADDING * 2, selection_obj,
+                                                                    float(parts[2])))
             prop_file_lines.append(os.linesep)
             try:
-                prop_file_lines.append("\t{}.bounds_limit = {}".format(selection_obj, float(parts[3])))
+                prop_file_lines.append("{}{}.bounds_limit = {}".format(PADDING * 2, selection_obj,
+                                                                       float(parts[3])))
                 prop_file_lines.append(os.linesep)
             except IndexError:
                 pass
 
-        prop_file_lines.append("\tself.sky_region.selections = {}{}{}".format("{",
+        prop_file_lines.append("{}self.sky_region.selections = {}{}{}".format(PADDING * 2, "{",
                                                                               ", ".join(selection_list),
                                                                               "}"))
         prop_file_lines.append(os.linesep)
 
         sky_region_combiners = str(self.field("sky_region_combiners").toString())
         if sky_region_combiners != "":
-            prop_file_lines.append("\tself.sky_region.combiners = "
-                                   "{}".format(sky_region_combiners.split(',')))
+            prop_file_lines.append("{}self.sky_region.combiners = "
+                                   "{}".format(PADDING * 2, sky_region_combiners.split(',')))
             prop_file_lines.append(os.linesep)
 
-        prop_file_lines.append("\t# ----------------------------")
+        prop_file_lines.append("{}# ----------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# Sky Exclusion specifications")
+        prop_file_lines.append("{}# Sky Exclusion specifications".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# ----------------------------")
+        prop_file_lines.append("{}# ----------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
 
-        prop_file_lines.append("\tself.sky_exclusion.dec_window "
-                               "= {}".format(str(self.field("sky_exclusions_dec_window").toString())))
+        prop_file_lines.append("{}self.sky_exclusion.dec_window "
+                               "= {}".format(PADDING * 2,
+                                             str(self.field("sky_exclusions_dec_window").toString())))
         prop_file_lines.append(os.linesep)
 
         sky_exclusion_selections = str(self.field("sky_exclusion_selections").toString()).strip()
         if sky_exclusion_selections != "":
             selection_obj = "excl0"
             parts = sky_exclusion_selections.split(',')
-            prop_file_lines.append("\t{} = Selection()".format(selection_obj))
+            prop_file_lines.append("{}{} = Selection()".format(PADDING * 2, selection_obj))
             prop_file_lines.append(os.linesep)
-            prop_file_lines.append("\t{}.limit_type = \"{}\"".format(selection_obj, parts[0]))
+            prop_file_lines.append("{}{}.limit_type = \"{}\"".format(PADDING * 2, selection_obj, parts[0]))
             prop_file_lines.append(os.linesep)
-            prop_file_lines.append("\t{}.minimum_limit = {}".format(selection_obj, float(parts[1])))
+            prop_file_lines.append("{}{}.minimum_limit = {}".format(PADDING * 2, selection_obj,
+                                                                    float(parts[1])))
             prop_file_lines.append(os.linesep)
-            prop_file_lines.append("\t{}.maximum_limit = {}".format(selection_obj, float(parts[2])))
+            prop_file_lines.append("{}{}.maximum_limit = {}".format(PADDING * 2, selection_obj,
+                                                                    float(parts[2])))
             prop_file_lines.append(os.linesep)
-            prop_file_lines.append("\t{}.bounds_limit = {}".format(selection_obj, float(parts[3])))
+            prop_file_lines.append("{}{}.bounds_limit = {}".format(PADDING * 2, selection_obj,
+                                                                   float(parts[3])))
             prop_file_lines.append(os.linesep)
-            prop_file_lines.append("\tself.sky_exclusion.selections = {}0: {}{}".format("{", selection_obj,
+            prop_file_lines.append("{}self.sky_exclusion.selections = {}0: {}{}".format(PADDING * 2, "{",
+                                                                                        selection_obj,
                                                                                         "}"))
             prop_file_lines.append(os.linesep)
 
-        prop_file_lines.append("\t# ---------------------------------")
+        prop_file_lines.append("{}# ---------------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# Sky Nightly Bounds specifications")
+        prop_file_lines.append("{}# Sky Nightly Bounds specifications".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# ---------------------------------")
-        prop_file_lines.append(os.linesep)
-
-        prop_file_lines.append("\tself.sky_nightly_bounds.twilight_boundary = {}"
-                               .format(str(self.field("sky_nightly_bounds_twilight_boundary").toString())))
+        prop_file_lines.append("{}# ---------------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
 
-        prop_file_lines.append("\tself.sky_nightly_bounds.delta_lst = {}"
-                               .format(str(self.field("sky_nightly_bounds_delta_lst").toString())))
+        prop_file_lines.append("{}self.sky_nightly_bounds.twilight_boundary = {}"
+                               .format(PADDING * 2,
+                                       str(self.field("sky_nightly_bounds_twilight_boundary").toString())))
         prop_file_lines.append(os.linesep)
 
-        prop_file_lines.append("\t# ------------------------------")
-        prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# Sky Constraints specifications")
-        prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# ------------------------------")
+        prop_file_lines.append("{}self.sky_nightly_bounds.delta_lst = {}"
+                               .format(PADDING * 2,
+                                       str(self.field("sky_nightly_bounds_delta_lst").toString())))
         prop_file_lines.append(os.linesep)
 
-        prop_file_lines.append("\tself.sky_constraints.max_airmass = {}"
-                               .format(str(self.field("sky_constraints_max_airmass").toString())))
+        prop_file_lines.append("{}# ------------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\tself.sky_constraints.max_cloud = {}"
-                               .format(str(self.field("sky_constraints_max_cloud").toString())))
+        prop_file_lines.append("{}# Sky Constraints specifications".format(PADDING * 2))
+        prop_file_lines.append(os.linesep)
+        prop_file_lines.append("{}# ------------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
 
-        prop_file_lines.append("\t# ----------------------")
+        prop_file_lines.append("{}self.sky_constraints.max_airmass = {}"
+                               .format(PADDING * 2,
+                                       str(self.field("sky_constraints_max_airmass").toString())))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# Scheduling information")
+        prop_file_lines.append("{}self.sky_constraints.max_cloud = {}"
+                               .format(PADDING * 2, str(self.field("sky_constraints_max_cloud").toString())))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# ----------------------")
+
+        prop_file_lines.append("{}# ----------------------".format(PADDING * 2))
+        prop_file_lines.append(os.linesep)
+        prop_file_lines.append("{}# Scheduling information".format(PADDING * 2))
+        prop_file_lines.append(os.linesep)
+        prop_file_lines.append("{}# ----------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
 
         max_num_targets = str(self.field("scheduling_max_num_targets").toString())
         if max_num_targets == "":
             max_num_targets = "100"
-        prop_file_lines.append("\tself.scheduling.max_num_targets "
-                               "= {}".format(max_num_targets))
+        prop_file_lines.append("{}self.scheduling.max_num_targets "
+                               "= {}".format(PADDING * 2, max_num_targets))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\tself.scheduling.accept_serendipity "
-                               "= {}".format(self.field("scheduling_accept_serendipity").toBool()))
+        prop_file_lines.append("{}self.scheduling.accept_serendipity "
+                               "= {}".format(PADDING * 2,
+                                             self.field("scheduling_accept_serendipity").toBool()))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\tself.scheduling.accept_consecutive_visits "
-                               "= {}".format(self.field("scheduling_accept_consecutive_visits").toBool()))
+        prop_file_lines.append("{}self.scheduling.accept_consecutive_visits "
+                               "= {}".format(PADDING * 2,
+                                             self.field("scheduling_accept_consecutive_visits").toBool()))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\tself.scheduling.airmass_bonus = {}"
-                               .format(str(self.field("scheduling_airmass_bonus").toString())))
+        prop_file_lines.append("{}self.scheduling.airmass_bonus = {}"
+                               .format(PADDING * 2, str(self.field("scheduling_airmass_bonus").toString())))
         prop_file_lines.append(os.linesep)
 
-        prop_file_lines.append("\t# --------------------------")
+        prop_file_lines.append("{}# --------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# Band Filter specifications")
+        prop_file_lines.append("{}# Band Filter specifications".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
-        prop_file_lines.append("\t# --------------------------")
+        prop_file_lines.append("{}# --------------------------".format(PADDING * 2))
         prop_file_lines.append(os.linesep)
 
         used_filters = []
@@ -222,30 +237,30 @@ class ProposalCreationWizard(QtGui.QWizard):
             use_filter = self.field("{}_use".format(field_stem)).toBool()
             if use_filter:
                 used_filters.append(("{}.name".format(field_stem), field_stem))
-                prop_file_lines.append("\t{} = BandFilter()".format(field_stem))
+                prop_file_lines.append("{}{} = BandFilter()".format(PADDING * 2, field_stem))
                 prop_file_lines.append(os.linesep)
-                prop_file_lines.append("\t{}.name = \'{}\'".format(field_stem, band_filter))
+                prop_file_lines.append("{}{}.name = \'{}\'".format(PADDING * 2, field_stem, band_filter))
                 prop_file_lines.append(os.linesep)
-                prop_file_lines.append("\t{}.num_visits = {}".format(field_stem,
+                prop_file_lines.append("{}{}.num_visits = {}".format(PADDING * 2, field_stem,
                                        str(self.field("{}_num_visits".format(field_stem)).toString())))
                 prop_file_lines.append(os.linesep)
-                prop_file_lines.append("\t{}.bright_limit = {}".format(field_stem,
+                prop_file_lines.append("{}{}.bright_limit = {}".format(PADDING * 2, field_stem,
                                        str(self.field("{}_bright_limit".format(field_stem)).toString())))
                 prop_file_lines.append(os.linesep)
-                prop_file_lines.append("\t{}.dark_limit = {}".format(field_stem,
+                prop_file_lines.append("{}{}.dark_limit = {}".format(PADDING * 2, field_stem,
                                        str(self.field("{}_dark_limit".format(field_stem)).toString())))
                 prop_file_lines.append(os.linesep)
-                prop_file_lines.append("\t{}.max_seeing = {}".format(field_stem,
+                prop_file_lines.append("{}{}.max_seeing = {}".format(PADDING * 2, field_stem,
                                        str(self.field("{}_max_seeing".format(field_stem)).toString())))
                 prop_file_lines.append(os.linesep)
-                prop_file_lines.append("\t{}.exposures = [{}]".format(field_stem,
+                prop_file_lines.append("{}{}.exposures = [{}]".format(PADDING * 2, field_stem,
                                        str(self.field("{}_exposures".format(field_stem)).toString())))
                 prop_file_lines.append(os.linesep)
 
         filters = []
         for i, used_filter in enumerate(used_filters):
             if i != 0:
-                line_padding = "\t\t\t"
+                line_padding = PADDING * 6
             else:
                 line_padding = ""
             filters.append("{}{}: {},".format(line_padding, used_filter[0], used_filter[1]))
@@ -253,7 +268,7 @@ class ProposalCreationWizard(QtGui.QWizard):
         filters[-1] = filters[-1].strip(',')
         filters_spec = os.linesep.join(filters)
 
-        prop_file_lines.append("\tself.filters = {}{}{}".format("{", filters_spec, "}"))
+        prop_file_lines.append("{}self.filters = {}{}{}".format(PADDING * 2, "{", filters_spec, "}"))
 
         with open(os.path.join(prop_save_dir, prop_file_name), 'w') as ofile:
             for prop_file_line in prop_file_lines:
