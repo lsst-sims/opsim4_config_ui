dd9f093751b1e2bf7cd87c913c11471d73358e76
Andrew Heyer <aheyer@lsst.org>
2017-11-10 17:28:30 -0700

    adding group_limit parameter to the GUI general proposal model

--- python/lsst/sims/opsim4/model/gen_prop_model.py
+++ python/lsst/sims/opsim4/model/gen_prop_model.py
@@ -15,7 +15,7 @@ class GeneralPropModel(ModelHelper):
         config_obj : lsst.sims.ocs.configuration.proposal.General instance
             The instance containing the area distribution proposal information.
         """
-        self.parameter_order = ["name", "sky_region", "sky_exclusion", "sky_nightly_bounds",
+        self.parameter_order = ["name", "group_limit","sky_region", "sky_exclusion", "sky_nightly_bounds",
                                 "sky_constraints", "scheduling", "filters"]
 
         ModelHelper.__init__(self, config_obj)
